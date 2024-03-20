dc29da8f7d188855e1dae5aad0d296eb4d59471d
Kevin Su <>
2024-03-21 10:51:02 +1100

    fix proxy data

--- ksu-stan-tv-app/src/data/index.ts
+++ ksu-stan-tv-app/src/data/index.ts
@@ -1,2 +1,1 @@
 export * from "./getPrograms";
-export * from "./getProxyData";
--- ksu-stan-tv-app/src/hooks/usePrograms.ts
+++ ksu-stan-tv-app/src/hooks/usePrograms.ts
@@ -1,28 +1,26 @@
-import { getPrograms, getProxyData } from "data";
 import { useEffect, useState } from "react";
 
-// const cachedProgramsData = new Proxy([], {
-//   get: async function (target: any) {
-//     if (!target.programsData) {
-//       // Fetch data if it's not cached
-//       target.programsData = await getPrograms();
-//     } else {
-//       console.log("Returning cached data...");
-//       // return target.data;
-//     }
+import { getPrograms } from "data";
 
-//     return target.programsData;
-//   },
-// });
+const cachedProgramsData = new Proxy([], {
+  get: async function (target: any) {
+    if (!target.programsData) {
+      // Fetch data if it's not cached
+      target.programsData = await getPrograms();
+    } else {
+      console.log("Returning cached data...");
+      // return target.data;
+    }
+
+    return target.programsData;
+  },
+});
 
 export function usePrograms() {
   const [programsData, setProgramsData] = useState<any>([]);
 
   useEffect(() => {
-    (async () =>
-      setProgramsData(
-        await getProxyData("programsData", getPrograms).programsData
-      ))();
+    (async () => setProgramsData(await cachedProgramsData.programsData))();
   }, []);
 
   return programsData;
