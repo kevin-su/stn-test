25bc5266826a5514aa9590b9e24d91b6a8e0ebd5
Kevin Su <>
2024-03-20 15:14:08 +1100

    add proxy cach data

--- ksu-stan-tv-app/src/hooks/usePrograms.ts
+++ ksu-stan-tv-app/src/hooks/usePrograms.ts
@@ -2,14 +2,25 @@ import { useEffect, useState } from "react";
 
 import { getPrograms } from "data";
 
+const cachedProgramsData = new Proxy([], {
+  get: async function (target: any) {
+    if (!target.programsData) {
+      // Fetch data if it's not cached
+      target.programsData = await getPrograms();
+    } else {
+      console.log("Returning cached data...");
+      // return target.data;
+    }
+
+    return target.programsData;
+  },
+});
+
 export function usePrograms() {
   const [programsData, setProgramsData] = useState<any>([]);
 
   useEffect(() => {
-    (async () => {
-      const data = await getPrograms();
-      setProgramsData(data);
-    })();
+    (async () => setProgramsData(await cachedProgramsData.programsData))();
   }, []);
 
   return programsData;
